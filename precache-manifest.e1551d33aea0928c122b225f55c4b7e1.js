e849c0f8b5502dbb89a753b67b2c863f7a3431fb
seanjiangsh <seanjiangsh@gmail.com>
2020-07-10 14:55:23 +0800

    Updates

---
+++ precache-manifest.e1551d33aea0928c122b225f55c4b7e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99cde2fc7b36d1f7d4e5f499909d43d",
+    "url": "/ReactPractice_Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "eb177079f609520d4ed3",
+    "url": "/ReactPractice_Monsters-Rolodex/static/css/main.d899574b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/ReactPractice_Monsters-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPractice_Monsters-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb177079f609520d4ed3",
+    "url": "/ReactPractice_Monsters-Rolodex/static/js/main.ecb1c5f5.chunk.js"
+  },
+  {
+    "revision": "91dcae9471375f54c8a8",
+    "url": "/ReactPractice_Monsters-Rolodex/static/js/runtime-main.714dad5f.js"
+  }
+]);
